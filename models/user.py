745786c864284d82f8d4d77e1521559c4f689138
abdullahomg77 <abdullahomg77@gmail.com>
2024-03-20 20:11:01 -0700

    last commit

--- models/user.py
+++ models/user.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""User Class."""
-from models.base_model import BaseModel
+""" User class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
-class User(BaseModel):
-    """User Representation
-
-    Attributes:
-        email (str): User Email.
-        password (str): User Password.
-        first_name (str): User first name
-        last_name (str): User last name
-    """
-
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class User(BaseModel, Base):
+    """USers class"""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
