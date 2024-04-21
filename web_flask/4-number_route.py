ee7d5427951a12e98d24543556cb16f033ff916f
Emgmbakri <mrm507415@gmail.com>
2024-04-21 20:03:02 +0400

    Is it a number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,38 +1,46 @@
 #!/usr/bin/python3
-"script displays 5 routes"
+""" 4. Add fourth view function that displays var only if is integer """
 
 from flask import Flask
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
-def hello_function():
-    "Return some text"
+def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
-def texter():
-    "return text"
+def hello():
+    """ Return other text. """
     return 'HBNB'
+
 
 @app.route('/c/<text>')
 def c_text(text):
-    "takes text as a parameter and return it"
+    """ replace text with variable. """
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
+
+@app.route('/python/')
 @app.route('/python/<text>')
-def python_text(text):
-    "takes text as a parameter and return it"
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
 
-@app.route('/number/<n>')
-def integer_num(n):
-    "function return a number if it is integer only"
-    if isinstance(n, int)
+
+@app.route('/number/<int:n>')
+def number_text(n):
+    """ replace with int only if given int. """
+    n = str(n)
     return '{} is a number'.format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
